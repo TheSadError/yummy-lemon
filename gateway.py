4c29c6fe680ea4d6c3ce9055166d188d66f90dbf
Error <88781785+TheSadError@users.noreply.github.com>
2022-07-17 14:57:34 +0400

    Create gateway.py

---
+++ gateway.py
@@ -0,0 +1,16 @@
+import requests 
+from colorama import *
+import sys 
+
+
+def gateway():
+    for i in range(1,255):
+        for j in range(1,255): 
+            url = f"http://192.168.{i}.{j}"
+            try : 
+                gateway = requests.get(url)
+                if gateway.status_code == 200:
+                    print(Fore.BLUE+f"[+] IP Adress : {url}")
+                    sys.exit(0)
+            except :
+                print(Fore.RED+f"[-] IP Adress : {url}")
